ea52cadb2eb546584991d8ce90297b8bd5c35882
MarcoMen-Temp <mrc_men@yahoo.ie>
2020-07-01 20:12:25 +0100

    Updates

---
+++ precache-manifest.cbeb9f4b2077d3b7896edb7f206aa38c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ab27fc2473183e3afcd85d17d03c25c",
+    "url": "/RoboRedux/index.html"
+  },
+  {
+    "revision": "d9ecd82fd9b6992cc086",
+    "url": "/RoboRedux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "40bf5c907165dd642426",
+    "url": "/RoboRedux/static/css/main.fb1b7ec7.chunk.css"
+  },
+  {
+    "revision": "d9ecd82fd9b6992cc086",
+    "url": "/RoboRedux/static/js/2.48b415d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RoboRedux/static/js/2.48b415d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40bf5c907165dd642426",
+    "url": "/RoboRedux/static/js/main.2559bead.chunk.js"
+  },
+  {
+    "revision": "f10d528ae2b90ecd3bc5",
+    "url": "/RoboRedux/static/js/runtime-main.270e2e73.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboRedux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
